de7aaee037ae266c317ce841c6621494082b16b5
Hoang Trong Phuc <jnp6751@gmail.com>
2026-05-26 11:26:27 +0700

    [update] tuan3

--- Lab-02/cipher/railfence/railfence_cipher.py
+++ Lab-02/cipher/railfence/railfence_cipher.py
@@ -3,7 +3,7 @@ class RailfenceCipher:
         pass
     
     def rail_fence_encrypt(self, plain_text, num_rails):
-        rails = [[] for i in range(num_rails)]
+        rails = [[] for _ in range(num_rails)]
         rail_index = 0
         direction = 1
         for char in plain_text:
@@ -12,9 +12,9 @@ class RailfenceCipher:
                 direction = 1
             elif rail_index == num_rails -1:
                 direction = -1
-                rail_index += direction
-            cipher_text = ''.json(''.join(rail) for rail in rails)
-            return cipher_text
+            rail_index += direction
+        cipher_text = ''.join(''.join(rail) for rail in rails)
+        return cipher_text
         
     def rail_fence_decrypt(self, cipher_text, num_rails):
         rail_lenghts = [0] * num_rails
@@ -27,24 +27,24 @@ class RailfenceCipher:
                 direction = 1
             elif rail_index == num_rails -1:
                 direction = -1
-                rail_index += direction
+            rail_index += direction
             
         rails = []
         start = 0 
         for lenght in rail_lenghts:
-            rail.append(cipher_text[start:start + lenght])
+            rails.append(cipher_text[start:start + lenght])
             start += lenght
         plain_text = ""
         rail_index = 0 
         direction = 1
         
         for _ in range(len(cipher_text)):
-            plain_text += rails[rail_index] [0]
-            rail[rail_index] = rails[rail_index][1:]
+            plain_text += rails[rail_index][0]
+            rails[rail_index] = rails[rail_index][1:]
             if rail_index == 0:
                 direction = 1
             elif rail_index == num_rails - 1:
                 direction = -1
-                rail_index += direction 
-        return cipher_text
+            rail_index += direction 
+        return plain_text
                      
